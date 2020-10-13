4480f0e1676b75b8153f3ee58d63763aae44e9bc
Randy <randyarbolaez0@gmail.com>
2020-10-13 15:48:57 -0400

    changed UI

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 
@@ -7,7 +7,8 @@ import * as postActions from "../store/actions/posts-actions";
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  color: #d8dad3;
+  color: #c8d0d8;
+  transition: 0.5s;
 `;
 
 const Description = styled.p`
@@ -16,30 +17,26 @@ const Description = styled.p`
   /* width: 33%; */
   width: 20vmax;
   height: 3.5vmax;
+  cursor: arrow;
 `;
 
 const Wrapper = styled.div`
-  /* Background color */
-  /* background-color: rgba(0, 0, 0, 0.3); */
-
-  /* You can use gradient background color such as
-    background: linear-gradient(rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.1) 100%); */
-
   /* Curved corners */
   border-bottom-left-radius: 5%;
-  border-top-left-radius: 90%;
+  border-top-left-radius: 25%;
   border-bottom-right-radius: 5%;
-  border-top-right-radius: 90%;
+  border-top-right-radius: 25%;
   flex: 0 1 20%;
   margin: 2vw 0 0 -2vw;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  padding: 4%;
+  padding: 0 4% 4% 4%;
   width: 20%;
   background: #495159;
   display: flex;
   flex-direction: column;
   word-wrap: break-word;
+
+  transition: 0.5s;
   button {
     background: none;
     border: none;
@@ -54,8 +51,16 @@ const Wrapper = styled.div`
   }
 
   &:hover {
-    background: #fbe7b5;
-    margin: 2vh 4vw;
+    border-left: 0.3vmax #fcf6bd solid;
+    border-bottom: 0.3vmax #fcf6bd solid;
+    border-right: 0.3vmax #fcf6bd solid;
+    box-shadow: 0 0 0 0;
+    background: #c8d0d8;
+    transition: 0.5s;
+    h1 {
+      color: #495159;
+      transition: 0.5s;
+    }
   }
 `;
 
@@ -64,32 +69,9 @@ const DateMade = styled.p`
   text-align: center;
 `;
 
-const LikeContainer = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  padding: 0%;
-  .right-span {
-    margin-left: 25%;
-  }
-  .left-span {
-    margin-right: 25%;
-  }
-`;
-
 const PostCard = ({ post }) => {
   const dispatch = useDispatch();
-  const [userId, setUserId] = useState(false);
   const isUserLoggedIn = useSelector((state) => state.auth);
-  let token = useSelector((state) => state.auth.token);
-
-  useEffect(() => {
-    if (!isUserLoggedIn.userId) {
-      console.log("NO USER");
-    }
-    if (isUserLoggedIn.userId) {
-      setUserId(post.usersThatLikedThePost.includes(isUserLoggedIn.userId));
-    }
-  }, [isUserLoggedIn]);
 
   let dateCreated = post.created_at;
   let d = new Date(dateCreated);
@@ -120,40 +102,6 @@ const PostCard = ({ post }) => {
     }
   };
 
-  const onlikeOrDislikePostHandler = (
-    title,
-    description,
-    likes,
-    usersThatLikedThePost,
-    postid
-  ) => {
-    if (userId) {
-      console.log("already liked");
-      return;
-    }
-    if (!isUserLoggedIn.userId) {
-      console.log("no user");
-      return;
-    }
-    if (isUserLoggedIn.userId) {
-      try {
-        dispatch(
-          postActions.updatePost(
-            title,
-            description,
-            likes,
-            usersThatLikedThePost,
-            postid,
-            token
-          )
-        );
-        window.location.reload();
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  };
-
   // console.log(post);
 
   return (
@@ -161,23 +109,6 @@ const PostCard = ({ post }) => {
       <Title>{post.title}</Title>
       <Description>{post.description}</Description>
       <DateMade>{`${month} ${day}, ${year}`}</DateMade>
-      <LikeContainer>
-        <span
-          onClick={() => {
-            onlikeOrDislikePostHandler(
-              post.title,
-              post.description,
-              post.likes,
-              post.usersThatLikedThePost,
-              post._id
-            );
-          }}
-          className="right-span"
-        >
-          <button>{userId ? "♥︎" : "♡"}</button>
-        </span>
-        <span className="left-span">{post.likes || 0}</span>
-      </LikeContainer>
       {isUserLoggedIn.userId === post.user ? (
         <button onClick={() => onHandleDelete(post._id)}>Delete</button>
       ) : null}
